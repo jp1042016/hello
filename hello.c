734c89e104037b4a228f85128f8b67f55a0b35e1
serickson1 <59945113+serickson1@users.noreply.github.com>
2020-01-16 08:47:04 -0600

    Scott Erickson (collaborator) updating code to show name and major

--- hello.c
+++ hello.c
@@ -1,12 +1,15 @@
 /* This code prints my name and major
 Name: Ben Rezac
-Date: 2020/01/14
+Date: 2020/01/16
+ - Collaborator: Scott Erickson
 */
 #include <stdio.h>
 
 int main(void) {
 	printf("Ben Rezac\n");
 	printf("Electrical Engineering\n");
+	printf("Collaborator: Scott Erickson\n");
+	printf("Major: Electrical Engineering\n");
 	return 0;
 }
 
